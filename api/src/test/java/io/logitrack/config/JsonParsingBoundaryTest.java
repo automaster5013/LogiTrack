551804c36710a3d30e8b7b5c43f9b9fe919ed0e9
Jupyter Lee <automaster5013@gmail.com>
2026-09-25 19:28:36 +0900

    Reject unknown JSON properties (#93)

--- api/src/test/java/io/logitrack/config/JsonParsingBoundaryTest.java
+++ api/src/test/java/io/logitrack/config/JsonParsingBoundaryTest.java
@@ -2,6 +2,7 @@ package io.logitrack.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.logitrack.order.DispatchOrderRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -18,5 +19,16 @@ class JsonParsingBoundaryTest {
 
     @Test void acceptsDistinctObjectKeys() {
         assertDoesNotThrow(()->mapper.readTree("{\"role\":\"VIEWER\",\"subject\":\"operator-a\"}"));
+    }
+
+    @Test void rejectsUnknownRequestProperties() {
+        assertThrows(JsonProcessingException.class, () -> mapper.readValue(
+                "{\"vehicleId\":\"TRUCK-01\",\"role\":\"ADMIN\"}", DispatchOrderRequest.class));
+    }
+
+    @Test void acceptsDeclaredRequestProperties() {
+        DispatchOrderRequest request = assertDoesNotThrow(() -> mapper.readValue(
+                "{\"vehicleId\":\"TRUCK-01\"}", DispatchOrderRequest.class));
+        assertEquals("TRUCK-01", request.vehicleId());
     }
 }
